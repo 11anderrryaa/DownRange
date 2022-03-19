3b4dd0f2ee59a12a013598d7fcd24298b9b02cf1
Ryan A. Anderson <ryan.anderson.dhhs@gmail.com>
2022-03-19 16:16:24 -0600

    Adding CollectionViewCell file and xib

---
+++ DownRange/Controller/GunCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  GunCollectionViewCell.swift
+//  DownRange
+//
+//  Created by Ryan Anderson on 3/19/22.
+//
+
+import UIKit
+import expanding_collection
+
+class GunCollectionViewCell: BasePageCollectionCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
